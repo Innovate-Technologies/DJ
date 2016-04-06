cf8279522ee7c54d463922cbeb8d2f15aa75d9be
Maartje Eyskens <meyskens@me.com>
2016-04-06 15:51:20 +0200

    Add ITFrame

---
+++ overlay-common/components/itframe/api.js
@@ -0,0 +1,69 @@
+var rest = require("restler")
+
+module.exports.getConfig = (username, callback) => {
+	rest.post("https://itframe.innovatete.ch/cast/config", {
+		timeout: 100000,
+		data: {
+			username: username,
+			token: global.env.ITFrameToken,
+		},
+	}).on("complete", (body) => {
+		callback(null, body);
+	}).on("timeout", () => {
+		callback(new Error("Timeout"))
+	})
+}
+
+module.exports.getAllMusic = (callback) => {
+	rest.get(djBaseUrl() + "all-songs", {
+		timeout: 100000,
+	}).on("complete", (body) => {
+		callback(null, body);
+	}).on("timeout", () => {
+		callback(new Error("Timeout"))
+	})
+}
+
+module.exports.getSongInfo = (id, callback) => {
+	rest.get(djBaseUrl() + "song/" + id, {
+		timeout: 100000,
+	}).on("complete", (body) => {
+		callback(null, body);
+	}).on("timeout", () => {
+		callback(new Error("Timeout"))
+	})
+}
+
+module.exports.getMusicForTag = (tag, callback) => {
+	rest.get(djBaseUrl() + "songs-with-tag/" + tag, {
+		timeout: 100000,
+	}).on("complete", (body) => {
+		callback(null, body);
+	}).on("timeout", () => {
+		callback(new Error("Timeout"))
+	})
+}
+
+module.exports.getClocks = (callback) => {
+	rest.get(djBaseUrl() + "all-clocks", {
+		timeout: 100000,
+	}).on("complete", (body) => {
+		callback(null, body);
+	}).on("timeout", () => {
+		callback(new Error("Timeout"))
+	})
+}
+
+module.exports.getIntervals = (callback) => {
+	rest.get(djBaseUrl() + "all-intervals", {
+		timeout: 100000,
+	}).on("complete", (body) => {
+		callback(null, body);
+	}).on("timeout", () => {
+		callback(new Error("Timeout"))
+	})
+}
+
+var djBaseUrl = () => {
+	return "https://itframe.innovatete.ch/dj/" + global.config.username + "/" + global.config.internal.dj.key + "/"
+}
