0056ed2a0334dc71ebb04bee2a282041c734467a
Maartje Eyskens <meyskens@me.com>
2016-11-26 21:25:50 +0100

    Make intervals used the populated songs array

--- src/components/playlists/intervals.js
+++ src/components/playlists/intervals.js
@@ -1,20 +1,13 @@
 import { getIntervals, getSongInfo } from "../itframe/api.js"
 import _ from "underscore"
 
-var songsForID = {}
-
 export default async (songsList) => {
-    var intervals = await getIntervals()
-    var currentIntervals = selectCurrentIntervals(intervals)
+    const intervals = await getIntervals()
+    const currentIntervals = selectCurrentIntervals(intervals)
     debug(currentIntervals)
     if (currentIntervals.length > 0) {
         for (let interval of currentIntervals) {
-            let promises = []
-            for (let songID of interval.songs) {
-                promises.push(addSongInfo(songID))
-            }
-            await Promise.all(promises)
-            songsList = insertInterval(songsList, interval)
+            insertInterval(songsList, interval)
         }
     }
     return songsList
@@ -57,11 +50,11 @@ const insertInterval = function (playlist, {songs, intervalType, songsAtOnce, ev
             for (var i = 0; i < songsAtOnce; i++) {
                 var intervalSong
                 if (intervalType === "random") {
-                    intervalSong = songsForID[_.shuffle(songs)[0]]
+                    intervalSong = _.shuffle(songs)[0]
                     intervalSong.ignoreSeperation = true;
                     newPlaylist.push(intervalSong)
                 } else if (intervalType === "order") {
-                    intervalSong = songsForID[songs[orderCount]]
+                    intervalSong = songs[orderCount]
                     intervalSong.ignoreSeperation = true;
                     newPlaylist.push(intervalSong)
                     orderCount++
@@ -70,8 +63,8 @@ const insertInterval = function (playlist, {songs, intervalType, songsAtOnce, ev
                     }
                 } else if (intervalType === "all") {
                     for (var songInterval of songs) {
-                        songsForID[songInterval].ignoreSeperation = true;
-                        newPlaylist.push(songsForID[songInterval])
+                        songInterval.ignoreSeperation = true;
+                        newPlaylist.push(songInterval)
                     }
                 }
             }
@@ -80,19 +73,3 @@ const insertInterval = function (playlist, {songs, intervalType, songsAtOnce, ev
     }
     return newPlaylist
 }
-
-/* let addSongInfo = (id) => new Promise((resolve) => {
-    getSongInfo(id).then((info) => {
-        if (info.available) {
-            songsForID[id] = info
-        }
-        resolve(true)
-    })
-})*/
-
-const addSongInfo = (id) => getSongInfo(id).then(info => {
-    if (info.available) {
-        songsForID[id] = info
-    }
-    return true
-})
