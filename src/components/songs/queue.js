f057e7e0352593a19a96b1574e1dcedc4a4a1a7f
Maartje Eyskens <meyskens@me.com>
2016-12-12 11:53:03 +0100

    Add queueManager

---
+++ src/components/songs/queue.js
@@ -0,0 +1,58 @@
+import EventEmitter from "events"
+import _ from "underscore"
+import clocks from "../playlists/clocks.js"
+import intervals from "../playlists/intervals.js"
+
+export default () => {
+    const exp = {}
+    let clocksCheckInterval = null
+
+    exp.queueEvents = new EventEmitter()
+    exp.queue = []
+    exp.currentSong = {}
+    exp.isWriting = false
+
+    exp.loadClocks = async () => {
+        exp.isWriting = true
+        exp.queue = exp.queue.concat(await intervals(await clocks()))
+        exp.isWriting = false
+        exp.queueEvents.emit("queueUpdate")
+        setClocksInterval()
+    }
+
+    exp.reloadClocks = async () => {
+        exp.isWriting = true
+        exp.queue = await intervals(await clocks())
+        exp.isWriting = false
+        exp.queueEvents.emit("queueReset")
+        setClocksInterval()
+    }
+
+    exp.loadEvent = async () => {
+        exp.queueEvents.emit("queueReset")
+        // do some more things
+    }
+
+    exp.queueEvents.on("playsSong", (song) => {
+        if (exp.queue[0] && exp.queue[0]._id === song._id) {
+            exp.isWriting = true
+            exp.currentSong = _.clone(global.queueManager.queue[0])
+            exp.queue.splice(0, 1)
+            exp.isWriting = false
+        }
+    })
+
+    const setClocksInterval = () => {
+        clearInterval(clocksCheckInterval)
+        clocksCheckInterval = setInterval(() => {
+            if (!global.queueManager.isWriting) {
+                if (global.queueManager.queue.length <= 2) {
+                    debug("Loading in more songs")
+                    global.queueManage.loadClocks()
+                }
+            }
+        }, 1000)
+    }
+
+    return exp
+}
--- src/dj.js
+++ src/dj.js
@@ -1,7 +1,5 @@
-import { getConfig } from "./components/itframe/api"
 import express from "express"
-
-global.isWritingQueue = false;
+import { getConfig } from "./components/itframe/api"
 
 if (process.env.username) {
     global.djconfig.username = process.env.username
@@ -20,7 +18,6 @@ require("http").createServer(app).listen(8080);
 
 (async () => {
     try {
-        global.isWritingQueue = true
         console.log("     _____        ___    \n    /  /::\\      /  /\\   \n   /  /:/\\:\\    /  /:/   \n  /  /:/  \\:\\  /__/::\\   \n /__/:/ \\__\\:| \\__\\/\\:\\  \n \\  \\:\\ /  /:/    \\  \\:\\ \n  \\  \\:\\  /:/      \\__\\:\\\n   \\  \\:\\/:/       /  /:/\n    \\  \\::/       /__/:/ \n     \\__\\/        \\__\\/  \n                         \n")
         console.log("Copyright 2015-2016 Innovate Technologies")
         console.log("------------------------------------")
@@ -31,25 +28,16 @@ require("http").createServer(app).listen(8080);
             process.env.TZ = global.config.timezone
         }
         // making sure Date() isn't called till now
+
         global.at = require("node-at")
         global.cron = require("cron").CronJob
+        global.queueManager = require("./components/songs/queue.js")()
+
+        await global.queueManager.loadClocks()
 
         global.connection = requireFromRoot("components/" + engine + "/connect.js")()
-        connection.loadClocks()
         connection.start()
-        global.isWritingQueue = false
     } catch (err) {
         debug(err)
     }
 })()
-
-
-// Check if the Clock has enough songs left to play
-setInterval(() => {
-    if (!global.isWritingQueue) {
-        if (global.connection.getQueue().length <= 2) {
-            debug("Loading in more songs")
-            global.connection.loadClocks()
-        }
-    }
-}, 1000)
