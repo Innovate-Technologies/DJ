494d87927f9ca589f67a68a276e6fbd7bd3adf8b
Maartje Eyskens <meyskens@me.com>
2016-12-12 14:28:36 +0100

    add socket.io updates on queue

--- src/components/api/main.js
+++ src/components/api/main.js
@@ -6,29 +6,64 @@ export default ({app, io}) => {
     });
     app.use("/api/:key/*", (req, res, next) => {
         if (req.params.key !== global.config.apikey) {
-            return res.status(401).send({status: "error", error: "Invalid key"})
+            return res.status(401).send({ status: "error", error: "Invalid key" })
         }
         next();
     });
 
-    app.get("/api/:key/queue", (req, res) => {
-        const queue = JSON.parse(JSON.stringify(global.queueManager.queue)) // dirty clone
-        for (let id in queue) {
-            if (queue.hasOwnProperty(id)) {
-                queue[id].internalURL = null
-                queue[id].processedURLS = null
-            }
-        }
-        res.json(queue)
+    app.get("/api/:key/songs/queue", (req, res) => {
+        res.json(getQueue())
     })
 
-    app.post("/api/:key/song/skip", (req, res) => {
+    app.get("/api/:key/songs/current", (req, res) => {
+        res.json(getCurrentSong())
+    })
+
+    app.post("/api/:key/songs/skip", (req, res) => {
         global.connection.skipSong()
-        res.json({status: "ok"})
+        res.json({ status: "ok" })
     })
 
     app.post("/api/:key/clocks/reload", (req, res) => {
         global.queueManager.reloadClocks()
-        res.json({status: "ok"})
+        res.json({ status: "ok" })
     })
+
+    io.of("/queueEvents").on("connection", (socket) => {
+        socket.on("key", (key) => {
+            if (key === global.config.apikey) {
+                socket.emit("currentSong", getCurrentSong())
+                socket.emit("queue", getQueue())
+                global.queueManager.queueEvents.on("queueUpdate", () => {
+                    socket.emit("queue", getQueue())
+                })
+                global.queueManager.queueEvents.on("queueReset", () => {
+                    socket.emit("queue", getQueue())
+                })
+                global.queueManager.queueEvents.on("playsSong", () => {
+                    socket.emit("currentSong", getCurrentSong())
+                    socket.emit("queue", getQueue())
+                })
+            } else {
+                socket.emit("error", "invalid key")
+            }
+        });
+    });
+}
+
+const getCurrentSong = () => {
+    const currentSong = JSON.parse(JSON.stringify(global.queueManager.currentSong)) // dirty clone
+    currentSong.internalURL = null
+    currentSong.processedURLS = null
+    return
+}
+
+const getQueue = () => {
+    const queue = JSON.parse(JSON.stringify(global.queueManager.queue)) // dirty clone
+    for (let id in queue) {
+        if (queue.hasOwnProperty(id)) {
+            queue[id].internalURL = null
+            queue[id].processedURLS = null
+        }
+    }
 }
