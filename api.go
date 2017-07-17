e5f6bd137d2fc91a38283fc05e41f64c8d66f176
Maartje Eyskens <meyskens@me.com>
2017-07-17 15:34:46 +0200

    add GET/POST api

--- api.go
+++ api.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"log"
 
+	"net/http"
+
 	socketio "github.com/googollee/go-socket.io"
 	"github.com/labstack/echo"
 )
@@ -14,9 +16,13 @@ func startServer() {
 		log.Fatal(err)
 	}
 	io.Of("/queueEvents").On("connection", handleQueueEvents)
-
 	e := echo.New()
+	e.GET("/api/:key/songs/queue", getQueue)
+	e.GET("/api/:key/songs/current", getCurrentSong)
+	e.POST("/api/:key/songs/skip", postSkip)
+	e.POST("/api/:key/clocks/reload", postReload)
 	e.Any("/socket.io/*", echo.WrapHandler(io))
+	e.Use(checkKey)
 	e.Logger.Fatal(e.Start(":80"))
 }
 
@@ -49,4 +55,31 @@ func sendQueue(socket socketio.Socket) {
 	jsonOut, _ := json.Marshal(queue)
 	socket.Emit("currentSong", string(jsonOut))
 	queueMutex.Unlock()
+}
+
+func checkKey(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		if c.Path()[:5] == "/api/" && c.Param("key") != conf.APIKey {
+			return c.JSON(http.StatusUnauthorized, map[string]string{"error": "bad API key"})
+		}
+		return next(c)
+	}
+}
+
+func getQueue(c echo.Context) error {
+	return c.JSON(http.StatusOK, queue)
+}
+
+func getCurrentSong(c echo.Context) error {
+	return c.JSON(http.StatusOK, currentSong)
+}
+
+func postSkip(c echo.Context) error {
+	events.Emit("skipSong")
+	return c.JSON(http.StatusOK, map[string]string{"status": "ok"})
+}
+
+func postReload(c echo.Context) error {
+	events.Emit("reloadQueue")
+	return c.JSON(http.StatusOK, map[string]string{"status": "ok"})
 }
