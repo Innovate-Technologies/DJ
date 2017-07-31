096fa3b1d2a1db1342a777a15b1ef96bdc03b62b
Maartje Eyskens <meyskens@me.com>
2017-07-31 11:48:03 +0200

    add extra fields to Interval

--- data/interval.go
+++ data/interval.go
@@ -12,4 +12,13 @@ type Interval struct {
 	Start        time.Time `json:"start"`
 	End          time.Time `json:"end"`
 	Forever      bool      `json:"forever"`
+	Days         []int     `json:"days"`
+	DayStart     struct {
+		Hour   int `json:"hour"`
+		Minute int `json:"minute"`
+	} `json:"dayStart"`
+	DayEnd struct {
+		Hour   int `json:"hour"`
+		Minute int `json:"minute"`
+	} `json:"dayEnd"`
 }
