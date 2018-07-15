2358b2ec2963a6b4503bdb60c750eda8b731cd09
Maartje Eyskens <meyskens@me.com>
2018-07-15 21:40:03 +0200

    update year

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ var (
 
 func main() {
 	fmt.Print("     _____        ___    \n    /  /::\\      /  /\\   \n   /  /:/\\:\\    /  /:/   \n  /  /:/  \\:\\  /__/::\\   \n /__/:/ \\__\\:| \\__\\/\\:\\  \n \\  \\:\\ /  /:/    \\  \\:\\ \n  \\  \\:\\  /:/      \\__\\:\\\n   \\  \\:\\/:/       /  /:/\n    \\  \\::/       /__/:/ \n     \\__\\/        \\__\\/  \n                         \n")
-	fmt.Println("Copyright 2017 SHOUTca.st")
+	fmt.Println("Copyright 2018 SHOUTca.st")
 	fmt.Println("=========================")
 
 	if username == "" {
